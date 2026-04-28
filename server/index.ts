b5ec827ac8997fd2fdec78450426ba0b61f5eabf
Sharif <sharif@Sharifs-MacBook-Air.local>
2026-04-28 22:05:02 +0600

    server port fixed

--- server/index.ts
+++ server/index.ts
@@ -111,15 +111,16 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  const host = process.env.HOST || "127.0.0.1";
-  httpServer.listen(
-    {
-      port,
-      host,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+  const port = parseInt(process.env.PORT || "4000", 10);
+const host = "0.0.0.0";
+
+httpServer.listen(
+  {
+    port,
+    host,
+  },
+  () => {
+    log(`serving on ${host}:${port}`);
+  }
+);
 })();
